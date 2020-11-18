b61ee052938d50139bb04b85aac6fcd9f6d3295e
Paulo Santiago <p.santiago796@gmail.com>
2020-11-18 22:00:58 +0000

    created loadTweets and changed initial-tweets.json

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -68,30 +68,30 @@
 
 
 // Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 const renderTweets = function(tweets) {
 // loops through tweets
@@ -133,13 +133,15 @@ $(document).ready(function() {
     );
   });
 
+  // const getTweets = require('../../server/lib/data-helpers');
   const loadTweets = () => {
     $.ajax("http://localhost:8080/tweets", { method: "GET" })
     .then(originalTweets => {
-      console.log("pls help", originalTweets);
+      // console.log("pls help", originalTweets);
+      renderTweets(originalTweets);
     });
   };
 
   loadTweets();
-  renderTweets(data);
+  // renderTweets(data);
 });
